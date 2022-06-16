3aa43cf77d431884d28ca489e6e66a17e5793d10
JAAlvarado-Montes <jaime-andres.alvarado-montes@students.mq.edu.au>
2022-06-16 22:12:47 +1000

    Changed path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 from distutils.util import convert_path
-sys.path.append(os.path.join(os.path.dirname(__name__), '../../'))
+sys.path.insert(0, os.path.abspath("../../"))
 
 
 extensions = [
